3f5066afd3e489bc6b49c3a6327f0063dec11188
ruzwn <ruzwn@users.noreply.github.com>
2021-11-18 18:35:58 +0300

    Update

--- SparseMatrix/Program.cs
+++ SparseMatrix/Program.cs
@@ -7,6 +7,12 @@ namespace SparseMatrix
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            SparseMatrix m1 = new SparseMatrix(5, 4);
+            Console.WriteLine(m1.Size);
+            m1.ChangeElem(1, 1, 8);
+            Console.WriteLine(m1[1, 1]);
+            Console.WriteLine(m1[5, 4]);
+            Console.WriteLine(m1);
         }
     }
 }
--- SparseMatrix/SparseMatrix.cs
+++ SparseMatrix/SparseMatrix.cs
@@ -1,38 +1,128 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 namespace SparseMatrix
 {
-	public class SparseMatrix // : ICollection
+	public class SparseMatrix : IEnumerable // IEnumerator
 	{
-		private readonly (int, int) _size = (row: 0, col: 0);
+		private readonly (int row, int col) _size;
 
-		public (int, int) Size { get; }
+		public (int, int) Size => _size;
 
-		private Dictionary<(int, int), int /* Template? */> _dictionary;
+		private readonly Dictionary<(int, int), int> elements;
 
-		public SparseMatrix()
+		private bool IsOutOfRange(int row, int col)
 		{
+			if (row == 0 || col == 0)
+				return true;
+			if (row > _size.row || col > _size.col)
+				return true;
+			
+			return false;
+		}
+		
+		public int this[int row, int col] // Доступ по индексу к матрице, а не массиву (не с 0, а с 1 начало)
+		{
+			get
+			{
+				(row, col) = (Math.Abs(row), Math.Abs(col));
+				if (IsOutOfRange(row, col))
+					throw new Exception("Index out of range");
+				
+				foreach (var (key, value) in elements)
+				{
+					if (key != (row, col)) continue;
+					return value;
+				}
+
+				return 0; // default element
+			}
+		}
+
+		public SparseMatrix() // ?
+		{
+			_size = (1, 1);
+			elements = new Dictionary<(int, int), int>(); // ?
 		}
 
 		public SparseMatrix(int row, int col)
 		{
+			row = Math.Max(Math.Abs(row), 1);
+			col = Math.Max(Math.Abs(col), 1);
 			_size = (row, col);
+			elements = new Dictionary<(int, int), int>(); // ?
 		}
 
 		public SparseMatrix(SparseMatrix other)
 		{
 			_size = other._size;
-			//_dictionary = other._dictionary.Copy;
+			elements = new Dictionary<(int, int), int>(other.elements);
+		}
+		
+		public bool ChangeElem(int row, int col, int value)
+		{
+			(row, col) = (Math.Abs(row), Math.Abs(col));
+			if (IsOutOfRange(row, col))
+				throw new Exception("Index out of range");
+			
+			foreach (var (key, i) in elements)
+			{
+				if (key != (row, col)) continue;
+				elements[key] = value;
+				return true;
+			}
+			
+			elements.Add((row, col), value);
+			return true;
 		}
 
-		public bool ChangeElem(int row, int col, int /* Template? */ value)
+		private void AddToNeighbors(Dictionary<(int, int), int> neighbors)
 		{
-			foreach (var elem in _dictionary)
+			for (int i = -1; i < 2; ++i)
 			{
-				if (elem.Key == (row, col))
-					elem.Key = value;
+				for (int j = -1; j < 2; ++j)
+				{
+					
+				}
 			}
+		}
+
+		public Dictionary<(int, int), int> GetNeighbors(int row, int col)
+		{
+			(row, col) = (Math.Abs(row), Math.Abs(col));
+			if (IsOutOfRange(row, col))
+				throw new Exception("Index out of range");
+
+			Dictionary<(int row, int col), int> neighbors = new Dictionary<(int, int), int>(); // ?
+			AddToNeighbors(neighbors);
+
+			return neighbors;
+		}
+
+		public override string ToString()
+		{
+			string str = string.Empty;
+			
+			for (int i = 1; i < _size.row + 1; ++i)
+			{
+				for (int j = 1; j < _size.col + 1; ++j)
+				{
+					str += this[i, j];
+					if (j + 1 < _size.col + 1)
+						str += "\t";
+				}
+				if (i + 1 < _size.row + 1)
+					str += "\n";
+			}
+
+			return str;
+		}
+
+		public IEnumerator GetEnumerator() // изменить для упорядоченного вывода соседей?
+		{
+			return elements.GetEnumerator();
 		}
 	}
 }
